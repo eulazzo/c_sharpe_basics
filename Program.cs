13d7bd5fa93b4c3f479484c82a0017f1a457dfb0
Lázaro Vanderson <eulazzo@protonmail.com>
2022-08-17 23:33:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSHARPCOURSE{
+    class Program{
+        static void Main(string[] args){
+            Console.Write("Type your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Which year has you born? ");
+            int yearOfBirth = int.Parse(Console.ReadLine());
+
+            Console.Write("Which year are we in? ");
+            int currentYear = int.Parse(Console.ReadLine());
+            int myAge  =currentYear - yearOfBirth;
+            Console.WriteLine($"Olá, {name}, Your age are {myAge} years old");            
+        
+            string isOverAge = (currentYear - yearOfBirth) > 18 ?"Your can drink" : "Sorry pal, maybe a few years later.";
+
+            Console.WriteLine(isOverAge);
+        }
+    }
+}
+
