ff89bf17bb64abd010918fe1f93a471dbbd41993
Lázaro Vanderson <eulazzo@protonmail.com>
2022-08-20 22:42:07 -0300

    guessing game

---
+++ 00 - GuessGame/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GuessGame{
+    class Program{
+        static void Main(string[] args){
+           string secretWord = "code";
+           string myGuessTry =  "";
+           int guessCount = 0;
+           int guessLimit = 3;
+           bool outOfGuesses = false;
+
+           while(secretWord!=myGuessTry && !outOfGuesses){
+              if(guessCount<guessLimit){
+                 Console.WriteLine("Try to guess my secret word: ");
+                 myGuessTry = Console.ReadLine();
+                 guessCount++;
+              }else {;
+                outOfGuesses = true;
+              }
+           } 
+
+           if(secretWord==myGuessTry){
+             Console.WriteLine("Your Win!");
+           }else{
+             Console.WriteLine("Ops! It's seems that your guesses chances has ended!");
+           }
+        }
+    } 
+}
+
+ 
+
+
+ 
+ 
+
+*/
--- Program.cs
+++ Program.cs
@@ -3,30 +3,10 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace GuessGame{
+namespace Pratice{
     class Program{
         static void Main(string[] args){
-           string secretWord = "code";
-           string myGuessTry =  "";
-           int guessCount = 0;
-           int guessLimit = 3;
-           bool outOfGuesses = false;
 
-           while(secretWord!=myGuessTry && !outOfGuesses){
-              if(guessCount<guessLimit){
-                 Console.WriteLine("Try to guess my secret word: ");
-                 myGuessTry = Console.ReadLine();
-                 guessCount++;
-              }else {;
-                outOfGuesses = true;
-              }
-           } 
-
-           if(secretWord==myGuessTry){
-             Console.WriteLine("Your Win!");
-           }else{
-             Console.WriteLine("Ops! It's seems that your guesses chances has ended!");
-           }
         }
     } 
 }
